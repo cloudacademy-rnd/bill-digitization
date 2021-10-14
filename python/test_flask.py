629c39989e6bd01873eb721871ea92b3a7ea9db0
Nguyen-Thanh <thanhch09091998@gmail.com>
2021-10-14 14:27:39 +0700

    BD-15 test

--- python/test_flask.py
+++ python/test_flask.py
@@ -2,8 +2,8 @@ from app import app
 
 def test_init_page():
     resp = app.test_client().get('/')
-    assert response.status_code == 200
+    assert resp.status_code == 200
 
 def test_about_page():
     resp = app.test_client().get('/about')
-    assert response.status_code == 200
+    assert resp.status_code == 200
