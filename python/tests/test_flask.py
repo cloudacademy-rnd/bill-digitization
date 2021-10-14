d5f67787edadfc9a61454b6b06edaa8984718336
Nguyen-Thanh <thanhch09091998@gmail.com>
2021-10-14 21:16:39 +0700

    BD-16 Build the Docker image

--- python/tests/test_flask.py
+++
@@ -1,9 +0,0 @@
-from flaskr import app
-
-def test_init_page():
-    resp = app.test_client().get('/')
-    assert resp.status_code == 200
-
-def test_about_page():
-    resp = app.test_client().get('/about')
-    assert resp.status_code == 200
